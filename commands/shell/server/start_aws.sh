ca66ea6743688d2924991e9b30d4ef6dacf67394
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2023-02-20 19:25:34 +0200

    Updated Shell scripts

--- commands/shell/server/start_aws.sh
+++ commands/shell/server/start_aws.sh
@@ -2,5 +2,4 @@
 startPort="4566"
 endPort="4571"
 
-echo "Starting LocalStack on http://localhost:$startPort/"
 docker run --rm -it -p $startPort:$startPort -p $endPort:$endPort localstack/localstack DYNAMODB_SHARE_DB=1
