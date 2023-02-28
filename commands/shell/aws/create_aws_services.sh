89b822ca8d49198a7cc6b325c17f85a9be30183f
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2023-02-28 17:39:28 +0200

    Updated the start environment shell script

--- commands/shell/aws/create_aws_services.sh
+++ commands/shell/aws/create_aws_services.sh
@@ -25,27 +25,39 @@ aws --endpoint-url=$endpointUrl s3api list-buckets
 echo \
 
 # Lambda #
-echo "Creating zip of resize-image Lambda"
-zip -r ./server/lambda/resize-image.zip ./server/
+echo "Creating zip for Lambda functions"
+zip -r server/lambda.zip server/
 echo \
 
-echo "Creating resize-image Lambda"
-aws --endpoint-url=$endpointUrl lambda create-function --function-name lambda-resize-image --zip-file fileb://server/lambda/resize-image.zip --handler ./server/lambda/resize-image.handler --runtime nodejs16.x --role arn:aws:iam::000000000000:role/lambda-role
+echo "Creating getMessages Lambda"
+aws --endpoint-url=$endpointUrl lambda create-function --function-name getMessages --zip-file fileb://server/lambda.zip --handler ./server/messages/getMessages.handler --runtime nodejs16.x --role arn:aws:iam::000000000000:role/lambda-role
+echo \
+
+echo "Creating sendMessage Lambda"
+aws --endpoint-url=$endpointUrl lambda create-function --function-name sendMessage --zip-file fileb://server/lambda.zip --handler ./server/messages/sendMessage.handler --runtime nodejs16.x --role arn:aws:iam::000000000000:role/lambda-role
+echo \
+
+echo "Creating SendImage Lambda"
+aws --endpoint-url=$endpointUrl lambda create-function --function-name sendImage --zip-file fileb://server/lambda.zip --handler ./server/messages/sendImage.handler --runtime nodejs16.x --role arn:aws:iam::000000000000:role/lambda-role
+echo \
+
+echo "Creating resizeImage Lambda"
+aws --endpoint-url=$endpointUrl lambda create-function --function-name resizeImage --zip-file fileb://server/lambda.zip --handler ./server/messages/resizeImage.handler --runtime nodejs16.x --role arn:aws:iam::000000000000:role/lambda-role
 echo \
 
 echo "Attaching trigger to resize-image Lambda"
-aws --endpoint-url=$endpointUrl s3api put-bucket-notification-configuration --bucket $bucketName --notification-configuration file://server/lambda/resize-image-trigger-config.json
+aws --endpoint-url=$endpointUrl s3api put-bucket-notification-configuration --bucket $bucketName --notification-configuration file://server/messages/resize-image-trigger-config.json
 echo \
 
 # SNS & SQS #
 echo "Creating SNS topic"
-aws --endpoint-url=$endpointUrl sns create-topic --name image-resize --region us-east-1 --output table | cat
+aws --endpoint-url=$endpointUrl sns create-topic --name imageResize --region us-east-1 --output table | cat
 echo \
 
 echo "Creaing SQS queue"
-aws --endpoint-url=$endpointUrl sqs create-queue --queue-name image-resize-messages --region us-east-1 --output table | cat
+aws --endpoint-url=$endpointUrl sqs create-queue --queue-name imageResizeMessages --region us-east-1 --output table | cat
 echo \
 
 echo "Subscribe SQS queue to SNS topic"
-aws --endpoint-url=$endpointUrl sns subscribe --topic-arn arn:aws:sns:us-east-1:000000000000:image-resize --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:image-resize-messages --output table | cat
+aws --endpoint-url=$endpointUrl sns subscribe --topic-arn arn:aws:sns:us-east-1:000000000000:imageResize --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:imageResizeMessages --output table | cat
 echo \
